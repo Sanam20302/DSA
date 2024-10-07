259da1c8dfac9dfc1f935739449ad2fd017ef535
Sanam20302 <76154182+Sanam20302@users.noreply.github.com>
2024-10-08 00:39:09 +0530

    Queue_LL.c

---
+++ Queue_LL.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int value;
+    struct node* next;
+}
+*temp,*front = NULL,*rear = NULL;
+void enqueue(int data){
+    temp = (struct node *)(malloc(sizeof(struct node)));
+    temp->value = data;
+    temp->next = NULL;
+    if(front==NULL || front>rear){
+        front = rear = temp;
+    }else{
+        rear->next = temp;
+        rear=temp;
+    }
+}
+void dequeue(){
+    if(front==NULL || front>rear){
+        printf("Empty\n");
+    }
+    else{
+        printf("Dequeued %d\n",front->value);
+        front = front->next;
+    }    
+}
+void display(){
+    if(front==NULL || front>rear){
+        printf("Empty\n");
+    }
+    else{
+        for (struct node* temp = front; temp != NULL; temp = temp->next){
+            printf("%d ", temp->value);
+        }
+    }
+}
+
+int main(){
+    enqueue(10);
+    enqueue(20);
+    dequeue();
+    dequeue();
+    dequeue();
+    enqueue(22);
+    enqueue(21);
+    display();
+    return 0;
+}
