d8cb410c0e8eaccf331ec0716db370325bf907f0
Sanam <76154182+Sanam20302@users.noreply.github.com>
2024-11-24 01:22:59 +0530

    Update Queue_Arr.c

--- Queue_Arr.c
+++ Queue_Arr.c
@@ -3,46 +3,61 @@
 #define max 5
 int arr[max],front = -1, rear = -1;
 void enqueue(int data){
-    if(rear==max-1){
+    if(rear == (max-1)){
         printf("Overflow\n");
-    }else{
-        front=0;
+    }
+    else if(front==-1){
+        front = rear = 0;
+        arr[rear]=data;
+        printf("Enqueued %d\n",data);
+    }
+    else{
         arr[++rear]=data;
+        printf("Enqueued %d\n",data);
     }
 }
 void dequeue(){
-    if(front==-1||front>rear){
-        printf("Empty\n");
+    if(front==-1){
+        printf("Underflow\n");
+    }
+    else if(front==rear){
+        printf("Dequeued %d\n",arr[front]);
+        front = rear = -1;
     }
     else{
         printf("Dequeued %d\n",arr[front]);
-        front+=1;
+        front++;
     }
-    if (front > rear) 
-        { 
-            front = rear = -1;
-        }
 
 }
 void display(){
-    if(front==-1||front>rear){
+    if(front==-1){
         printf("Empty\n");
     }
     else{
         for (int i=front;i<=rear;i++){
             printf("%d ", arr[i]);
         }
+        printf("\n");
     }
 }
 
 int main(){
     enqueue(10);
     enqueue(20);
-    dequeue();
-    dequeue();
-    dequeue();
-    enqueue(22);
     enqueue(21);
+    enqueue(22);
+    display();
+    dequeue();
+    dequeue();
+    dequeue();
+    display();
+    enqueue(33);
+    enqueue(21);
+    display();
+    dequeue();
+    dequeue();
+    dequeue();
     display();
     return 0;
 }
